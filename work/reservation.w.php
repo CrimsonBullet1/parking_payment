56826b2117309889327afb9e693f2e6f6408bf66
Iman-nn <aimanrosli0175@gmail.com>
2023-07-17 01:54:19 +0800

    Update file

--- work/reservation.w.php
+++ work/reservation.w.php
@@ -2,63 +2,55 @@
 
 session_start();
 include("../conn.php");
-
-if(isset($_POST["btn-reserve"])){
+PRINT_R($_POST);
 // Get the form input values
-    $slotnum = $_POST['slotnum'];
-    $reservation_date = $_POST['reservation_date'];
 // Retrieve other reservation details from $_POST
-    
-// Prepare the SELECT statement
+if(isset($_POST["button"])){
+    $parkingid = $_POST ['parkingid'];
+    $reservation_date = $_POST['reservation_date'];
+    $duration = $_POST['duration'];
+    $totalcost = $_POST['totalcost'];
+        
+    // Prepare the SELECT statement
     $query = "SELECT COUNT(*)
             FROM PARKING_LOTS
-            LEFT JOIN RESERVATIONS ON PARKING_LOTS.parkingid = RESERVATIONS.parkingid
-            WHERE slotnum = :slotnum
-            AND reservation_date = :reservation_date";
-    $statement = oci_parse($dbconn, $query);
-    
-// Process the reservation and save it to the database
-oci_bind_by_name($statement, ':slotnum', $slotnum);
-oci_bind_by_name($statement, ':reservation_date', $reservation_date);
-oci_execute($statement);
-$row = oci_fetch_assoc($statement);
+            WHERE slotnum = :slotnum";
+            $statement = oci_parse($dbconn, $query);
+            oci_bind_by_name($statement, ':slotnum', $slotnum);
+            oci_execute($statement);
+            $row = oci_fetch_assoc($statement);
 
         if ($row['COUNT'] > 0) {
-            echo "<script>alert('Rserved!<br> Please try with a different Slot');
+            echo "<script>alert('Reserved!<br> Please try with a different Slot');
             window.history.back();
             </script>";
         } 
         else {
-        $query = "INSERT INTO PARKING_LOTS(parkingid,slotnum) 
-            VALUES (:parkingid,:slotnum) 
-            AND 
-            INSERT INTO RESERVATIONS(reservationid,duration,totalcost,reservation_date) 
-            VALUES (:reservationid,:duration,:totalcost,:reservation_date);
-        $statement = oci_parse($dbconn, $query)";
+            // Insert to Reservation
+            $query = "INSERT INTO RESERVATIONS (PARKINGID, DURATION, TOTALCOST, RESERVATION_DATE) 
+                        VALUES (:parkingid, :duration, :totalcost, :reservation_date)";
 
-        // Bind the parameter value
-        oci_bind_by_name($statement, ':parkingid', $parkingid);
-        oci_bind_by_name($statement, ':slotnum', $slotnum);
-        oci_bind_by_name($statement, ':reservationid', $reservationid);
-        oci_bind_by_name($statement, ':duration', $duration);
-        oci_bind_by_name($statement, ':totalcost', $totalcost);
-        oci_bind_by_name($statement, ':reservation_date', $reservation_date);
+            $statement = oci_parse($dbconn, $query);
 
-            // Execute the statement
+            // Bind the parameter value
+            oci_bind_by_name($statement, ':parkingid', $parkingid);
+            oci_bind_by_name($statement, ':duration', $duration);
+            oci_bind_by_name($statement, ':totalcost', $totalcost);
+            oci_bind_by_name($statement, ':reservation_date', $reservation_date);
+
             if (oci_execute($statement)) {
-                echo "<script>alert('Successfully Reserving!');
-                window.location.href = '../parking_lot.php';
-                </script>";
+                echo "Data inserted into RESERVATIONS table successfully.";
             } 
             else {
-                $e = oci_error($statement);
-            echo "Error: " . $e['message'];
+                $error = oci_error($statement);
+                echo "Failed to insert data into RESERVATIONS table. Error: " . $error['message'];
             }
         }
-        }
-        else{
-        echo "<script>alert('Please try again!');
-        window.location.href = '../parking_lot.php';
+    }
+    
+    else {
+        echo "<script>
+        alert('Please try again!');
         </script>";
-        }
+    }
 ?>
