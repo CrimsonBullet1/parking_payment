c7b380827eda3ceac12d0cfffeaeabfe2bd77396
DauntLesS21 <najmuddiniqbal@gmail.com>
2023-07-17 01:55:14 +0800

    Iqbal commit

--- reservation.php
+++ reservation.php
@@ -4,14 +4,14 @@ include('config.php');
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Parking Lot Reservation Application</title>
+    <title>Reservation</title>
 
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="description" content=""/>
     <meta name="author" content=""/>
-    <title>Dashtreme Admin - Free Dashboard for Bootstrap 4 by Codervent</title>
+    <title>Car Park</title>
     <!-- loader-->
     <link href="assets/css/pace.min.css" rel="stylesheet"/>
     <script src="assets/js/pace.min.js"></script>
